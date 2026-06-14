d2df9d8a3ac121efa6ae8540c5e1f67f8328e765
krismalayao <kristopher.s.malayao@gmail.com>
2026-06-14 22:37:33 +0800

    ADDED: Registration Page
    Mobile Responsiveness for register

--- frontEnd/script/features.js
+++ frontEnd/script/features.js
@@ -1,3 +1,5 @@
+// Toggling See Password Button/Icon
+
 const passwordInput = document.getElementById('password');
 const toggleButton = document.getElementById('toggle-button');
 const toggleIcon = document.getElementById('toggle-icon');
@@ -10,4 +12,19 @@ toggleButton.addEventListener('click', function () {
         passwordInput.type = 'password';
         toggleIcon.className = 'bx bxs-lock-alt';
     }
+});
+
+// Script for Mobile Number Input
+const phoneInput = document.getElementById('phone_number');
+
+phoneInput.addEventListener('input', function (e) {
+    let value = e.target.value.replace(/\D/g, '');
+    
+    if (value.length > 3 && value.length <= 6) {
+        value = value.slice(0, 3) + '-' + value.slice(3);
+    } else if (value.length > 6) {
+        value = value.slice(0, 3) + '-' + value.slice(3, 6) + '-' + value.slice(6, 10);
+    }
+    
+    e.target.value = value;
 });
