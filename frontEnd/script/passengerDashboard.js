1e611e6b927a68ccda0488d849f6e804254e2157
LuisGabrielLwin <luis_lwin@dlsu.edu.ph>
2026-07-11 12:46:12 +0800

    Changed Chart

--- frontEnd/script/passengerDashboard.js
+++ frontEnd/script/passengerDashboard.js
@@ -4,10 +4,10 @@ const ridesCtx = document.getElementById('rides-chart');
 new Chart(ridesCtx, {
   type: 'bar',
   data: {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     datasets: [{
       label: 'Rides',
-      data: [5, 8, 6, 10, 7, 4]
+      data: [5, 8, 6, 10, 7, 4, 1, 0, 0, 0, 0, 0]
     }]
   },
   options: {
